ad961fa40ba22a02afa06800bc6fb89db4b0b33d
Areeba Fatima <afsbibi@gmail.com>
2025-05-30 16:53:37 +0500

    deploy error
    deploy error

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -13,7 +13,7 @@ const firebaseConfig = {
   measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
-
+console.log("Firebase Config", firebaseConfig);
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
